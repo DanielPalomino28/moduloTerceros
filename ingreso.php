06a3db9ba38565a36fe74b63fafd5bf633e5e749
DAPR <101226963+DanielPalomino28@users.noreply.github.com>
2023-08-14 22:46:50 -0500

    Cargue archivos

---
+++ ingreso.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bienvenida</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<h1>Bienvenido al sitio web</h1>
+    <h2>Seleccione la opción a realizar al lado izquierdo en el menú.</h2>
+
+	
+</body>
+</html>
