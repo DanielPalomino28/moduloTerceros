597c9101284193648b5cfe19c514de939aa1e35d
DAPR <101226963+DanielPalomino28@users.noreply.github.com>
2023-08-18 01:34:16 -0500

    Corrección texto en botón de la página principal

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
           <p class="col-md-8 fs-4">Para: Camilo Restrepo</p>
           <p class="col-md-8 fs-4">Por: Daniel Palomino</p>
           <p class="col-md-8 fs-4">Fecha: Viernes 18 de agosto de 2023</p>
-          <button class="btn btn-primary btn-lg" type="button" onclick="location.href='modulos/tercero/index.php'">Módulo terceros</button>
+          <button class="btn btn-primary btn-lg" type="button" onclick="location.href='modulos/tercero/index.php'">Listado de terceros</button>
         </div>
       </div>
 
